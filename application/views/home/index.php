e577292ca1c1562b2692f26f9fd6255054ec55f2
putriyani <yaniputeri26@gmail.com>
2021-01-06 19:49:48 +0800

    First commit

---
+++ application/views/home/index.php
@@ -0,0 +1,23 @@
+<!-- Carousel -->
+<div id="carouselExampleFade" class="carousel slide carousel-fade bg-light" data-ride="carousel">
+  <div class="container">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-5">
+            <h4>Humscrew adalah Coffeeshop <br>yang tepat untuk bersantai <br>dan bersenang ria</h4>
+            <p>"Jadikanlah Humscrew Rumah ke-2 kalian untuk bersantai dan bercengkrama dengan sekitar kalian. Dapatkan kejutan tak terduga dari Barista kami!"</p>
+            <a href="<?= base_url('menu'); ?>" class="btn btn-order">Order Now <i class="fas fa-arrow-right ml-3"></i></a>
+          </div>
+          <div class="col-lg-5 d-flex justify-content-end">
+            <div class="img-bg">
+              <img src="<?= base_url('assets/img/humscrew.png'); ?>"height:2000px; with:3001px;>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Carousel -->
+
